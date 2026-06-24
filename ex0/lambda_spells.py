5e6fdb13dde3186645c670a3e36c3912c16bbee5
blast <jugaitamers@gmail.com>
2026-06-24 20:18:04 +0200

    gl

---
+++ ex0/lambda_spells.py
@@ -0,0 +1,9 @@
+from typing import Callable
+
+artifacts = [{'name': 'Fire Staff', 'power': 71, 'type': 'accessory'},
+             {'name': 'Water Chalice', 'power': 108, 'type': 'weapon'},
+             {'name': 'Lightning Rod', 'power': 103, 'type': 'armor'},
+             {'name': 'Light Prism', 'power': 61, 'type': 'weapon'}]
+
+
+tri = lambda x: list[dict[str, int]] sorted()
